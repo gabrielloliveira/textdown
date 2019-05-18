ae59609eadb17d49695499708c1fcee3cfdbb351
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-05-18 13:20:41 -0300

    added base parsers to html tags for markdown

--- textdown.js
+++ textdown.js
@@ -2,6 +2,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const content_editor_element = document.querySelector('[contenteditable]');
   let result_markdown_element = document.getElementById('md-result')
   content_editor_element.addEventListener('input', function(){
-    result_markdown_element.textContent = content_editor_element.innerHTML
+    let md = htmlToMD(content_editor_element.innerHTML)
+    result_markdown_element.innerHTML = md
   })
 })
+
+function htmlToMD(text){
+  let parser = text.split(/[<>]/);
+  text = tagConversion(parser)
+  return text
+}
+
+function tagConversion(parser){
+  parser.forEach(function(element, index, parser){
+    switch(element){
+      case 'div':
+        parser[index] = ''
+        break
+      case '/div':
+        parser[index] = '<br>'
+        break
+      case 'br':
+        parser[index] = "<br>"
+        break
+      }
+  })
+  return parser.join('')
+}
