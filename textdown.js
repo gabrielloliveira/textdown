86ccb392445211abe64da1efd6b6a7497381a02a
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-05-18 15:11:09 -0300

    added execCommand for text

--- textdown.js
+++ textdown.js
@@ -1,22 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  const content_editor_element = document.querySelector('[contenteditable]');
+  const content_editor_element = document.querySelector('[contenteditable]')
   const options_buttons = document.getElementsByClassName('text-option-button')
   let result_markdown_element = document.getElementById('md-result')
+  
   content_editor_element.addEventListener('click', function(){
     if(content_editor_element.hasAttribute("first-time")){
       content_editor_element.removeAttribute('first-time')
       content_editor_element.textContent = ''
     }
   })
+  
+  for(let i = 0; i < options_buttons.length; i++){
+    options_buttons[i].addEventListener('click', function(){
+      options_buttons[i].classList.toggle('is-link')
+      if(options_buttons[i].classList.contains('is-link')){
+        document.execCommand(options_buttons[i].getAttribute('exec-command'), false, '')
+      }
+    })
+  }
+  
   content_editor_element.addEventListener('input', function(){
     let md = htmlToMD(content_editor_element.innerHTML)
     result_markdown_element.innerHTML = md
   })
-  for(let i = 0; i < options_buttons.length; i++){
-    options_buttons[i].addEventListener('click', function(){
-      options_buttons[i].classList.toggle('is-link')
-    })
-  }
 })
 
 function htmlToMD(text){
