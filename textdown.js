0375ce8b0989f8d507ffe999d4de12e663e94715
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2019-05-18 15:42:45 -0300

    added bold command for markdown results; added validations added validations for the execution of commands

--- textdown.js
+++ textdown.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const options_buttons = document.getElementsByClassName('text-option-button')
   let result_markdown_element = document.getElementById('md-result')
   
-  content_editor_element.addEventListener('click', function(){
+  content_editor_element.addEventListener('focus', function(){
     if(content_editor_element.hasAttribute("first-time")){
       content_editor_element.removeAttribute('first-time')
       content_editor_element.textContent = ''
@@ -14,16 +14,38 @@ document.addEventListener("DOMContentLoaded", function(event) {
     options_buttons[i].addEventListener('click', function(){
       options_buttons[i].classList.toggle('is-link')
       if(options_buttons[i].classList.contains('is-link')){
-        document.execCommand(options_buttons[i].getAttribute('exec-command'), false, '')
+        if(isFirstTime()){
+          content_editor_element.focus()
+          document.execCommand(options_buttons[i].getAttribute('exec-command'), false, '')
+        }else 
+          document.execCommand(options_buttons[i].getAttribute('exec-command'), false, '')
+      }else{
+        document.execCommand('removeFormat', false, '')        
       }
     })
-  }
+  }  
   
   content_editor_element.addEventListener('input', function(){
+    if (content_editor_element.textContent == '')
+      activateCommands()
+
     let md = htmlToMD(content_editor_element.innerHTML)
     result_markdown_element.innerHTML = md
   })
 })
+
+function activateCommands(){
+  const options_buttons_active = document.getElementsByClassName('text-option-button is-link')
+  for(let i = 0; i < options_buttons_active.length; i++){
+      document.execCommand(options_buttons_active[i].getAttribute('exec-command'), false, '')
+  }
+}
+
+function isFirstTime(){
+  if(document.querySelector('[contenteditable]').hasAttribute("first-time"))
+    return 1
+  return 0
+}
 
 function htmlToMD(text){
   let parser = text.split(/[<>]/);
@@ -42,6 +64,12 @@ function tagConversion(parser){
         break
       case 'br':
         parser[index] = "<br>"
+        break
+      case 'b':
+        parser[index] = '**'
+        break
+      case '/b':
+        parser[index] = '**'
         break
       }
   })
